b9222a79765366c6b3665d0df646ec6a40b3ec97
chantelle31 <cchantelle131@gmail.com>
2026-04-03 02:22:13 +0800

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exerices2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] numbers = new double[3];
+            for(int i=0;i<3;i++)
+            {
+                Console.WriteLine("請輸入第{i+1}個數字");
+                numbers[i] = Convert.ToDouble(Console.ReadLine());
+            }
+            Array.Sort(numbers);
+            Array.Reverse(numbers);
+            Console.WriteLine("\nfinal");
+            foreach (double num in numbers)
+            {
+                Console.WriteLine(num);
+            }
+               
+
+        }
+    }
+}
